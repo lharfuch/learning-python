c8ce324b86de928b8183f977b030a6bd58318ae8
Leandro Harfuch <harfuch@gmail.com>
2017-11-21 19:51:20 +0000

    Test with Class

--- beginner/UnitTest/name_function.py
+++ beginner/UnitTest/name_function.py
@@ -2,3 +2,8 @@ def get_formatted_name(first, last):
     """Generate a neatly formatted full name."""
     full_name = first + ' ' + last
     return full_name.title()
+    
+def get_formatted_full_name(first, middle, last):
+    """Generate a neatly formatted full name."""
+    full_name = first.strip() + ' ' + middle.strip() + ' ' + last.strip()
+    return full_name.title()
--- beginner/UnitTest/test_name_function.py
+++ beginner/UnitTest/test_name_function.py
@@ -1,5 +1,5 @@
 import unittest
-from name_function import get_formatted_name
+from name_function import get_formatted_name, get_formatted_full_name
 
 class NamesTestCase(unittest.TestCase):
     """Tests for 'name_function.py'."""
@@ -7,6 +7,10 @@ class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         """Do names like 'Janis Joplin' work?"""
         formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+        
+    def test_first_middle_last_name(self):
+        formatted_name = get_formatted_full_name('janis', 'a','joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
         
 unittest.main()
